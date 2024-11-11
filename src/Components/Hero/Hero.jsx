3f25d1473dbe86a1d1fb970001a7082164213e57
jamaisseule <nganttqgcd201774@fpt.edu.vn>
2024-11-11 09:47:06 +0700

    fix

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,23 +1,20 @@
-import React from 'react'
-import './Hero.css'
-import profile_img from '../../assets/profile_img.jpg'
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.jpg";
 
 const Hero = () => {
   return (
-    <div className='hero'>
-    <img src={profile_img} alt=""/>
-    <h1>Hi, It's Ngan Truong,</h1>
-    <p>I am frontend developer based in Vietnam.</p>
-    <div className="hero-action">
-      <a href="#about" className="hero-about">
-      <div >
-            About Me
-        </div>
-      </a>
-       
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>Hi, It's Ngan Truong,</h1>
+      <p>I am frontend developer based in Vietnam.</p>
+      <div className="hero-action">
+        <a href="#about" className="hero-about">
+          <div>About Me</div>
+        </a>
+      </div>
     </div>
-</div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
